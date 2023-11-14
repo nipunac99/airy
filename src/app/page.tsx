81469b0db167d973a8ef2e1c79d904140147fc4e
nipunac99 <nipunachamod99723@gmail.com>
2023-11-14 15:15:28 +0530

    creating-the-navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
           Chat with your{' '}
-          <span className='text-pink-600'>documents</span>{' '}
+          <span className='text-rose-600'>documents</span>{' '}
           in seconds.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
@@ -87,7 +87,7 @@ export default function Home() {
         <div className='mb-12 px-6 lg:px-8'>
           <div className='mx-auto max-w-2xl sm:text-center'>
             <h1 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
-              Start <span className='text-pink-600'>chatting</span> in minutes.
+              Start <span className='text-rose-600'>chatting</span> in minutes.
             </h1>
             <p className='mt-4 text-lg text-gray-600'>
               Chatting to your PDF files has never been
@@ -100,7 +100,7 @@ export default function Home() {
         <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
           <li className='md:flex-1'>
             <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-pink-600'>
+              <span className='text-sm font-medium text-rose-600'>
                 Step 1
               </span>
               <span className='text-xl font-semibold'>
@@ -111,7 +111,7 @@ export default function Home() {
                 choose our{' '}
                 <Link
                   href='/pricing'
-                  className='text-pink-700'>
+                  className='text-rose-700'>
                   pro plan.
                 </Link>
                 
@@ -120,7 +120,7 @@ export default function Home() {
           </li>
           <li className='md:flex-1'>
             <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-pink-600'>
+              <span className='text-sm font-medium text-rose-600'>
                 Step 2
               </span>
               <span className='text-xl font-semibold'>
@@ -134,7 +134,7 @@ export default function Home() {
           </li>
           <li className='md:flex-1'>
             <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-pink-600'>
+              <span className='text-sm font-medium text-rose-600'>
                 Step 3
               </span>
               <span className='text-xl font-semibold'>
