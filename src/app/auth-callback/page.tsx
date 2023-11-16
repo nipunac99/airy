fc00b505e5858203714908046221678104311be9
nipunac99 <nipunachamod99723@gmail.com>
2023-11-16 14:22:17 +0530

    creating-our-database

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,4 +1,5 @@
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
 
 const Page = async () => {
         const router = useRouter()
@@ -6,9 +7,19 @@ const Page = async () => {
         const searchParams = useSearchParams()
         const origin = searchParams.get('origin')
 
-        const apiResponse = await fetch('/api/whatever')
+        // const apiResponse = await fetch('/api/whatever')
 
-        const data = await apiResponse.json()
+        // const data = await apiResponse.json()
+
+        const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+                onSuccess: ({success}) => {
+                        if(success) {
+                                // user is synced to bd
+
+                                router.push(origin ? `/${origin}` : '/dashboard')
+                        }
+                }
+        })
 
 }
 
